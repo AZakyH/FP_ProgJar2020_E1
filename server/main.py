8a214e8fdf0b4315487b7c7eb24c52b5d9ed10de
bagasys <37441075+bagasys@users.noreply.github.com>
2020-05-14 18:25:16 +0700

    Create main.py server
    file untuk program server

---
+++ server/main.py
@@ -0,0 +1,10 @@
+class Server:
+    def __init__(self):
+        print('init')
+
+    def start(self):
+        print('start')
+
+if __name__ == '__main__':
+    server = Server()
+    server.start()
