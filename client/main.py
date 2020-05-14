00de19e93967c980d788163239e882b18f26fb3f
bagasys <37441075+bagasys@users.noreply.github.com>
2020-05-14 18:21:13 +0700

    welcome phase

--- client/main.py
+++ client/main.py
@@ -3,32 +3,55 @@ import pygame
 class Game:
     def __init__(self):
         print('init')
+        self.id = -1
         self.CAPTION = "BINGO!"
         self.SCREEN_RESOLUTION = (1280, 720)
         pygame.init()
         pygame.display.set_caption(self.CAPTION)
-        pygame.display.set_mode(self.SCREEN_RESOLUTION)
+        self.screen = pygame.display.set_mode(self.SCREEN_RESOLUTION)
 
         self.GAME_STATE = 1
-        self.WELCOME_STATE = 1
-        self.PLAY_STATE = 2
-        self.WINNER_STATE = 3
+        self.STATE_WELCOME = 1
+        self.STATE_PLAY = 2
+        self.STATE_WINNER = 3
+        self.count_player = 1
+        self.run = True
 
     def start(self):
         print('start')
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-            if self.GAME_STATE == self.WELCOME_STATE:
+        while self.run:
+            if self.GAME_STATE == self.STATE_WELCOME:
                 self.handle_welcome()
-            if self.GAME_STATE == self.PLAY_STATE:
+            if self.GAME_STATE == self.STATE_PLAY:
                 self.handle_play()
-            if self.GAME_STATE == self.WINNER_STATE:
+            if self.GAME_STATE == self.STATE_WINNER:
                 self.handle_winner()
 
+        pygame.quit()
+
     def handle_welcome(self):
-        pass
+        welcome_text = "Welcome to BINGO!"
+        welcome_font = pygame.font.SysFont('comicsans', 30, True)
+        welcome_surface = welcome_font.render(welcome_text, 1, (255,255,255))
+
+        player_text = "player {}...".format(self.count_player)
+        player_font = pygame.font.SysFont('comicsans', 50, True)
+        player_surface = player_font.render(player_text, 0, (255, 255, 255))
+
+        waiting_text = "waiting player-({}/5)...".format(self.count_player)
+        waiting_font = pygame.font.SysFont('comicsans', 50, True)
+        waiting_surface = waiting_font.render(waiting_text, 0, (255, 255, 255))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.run = False
+        self.screen.fill((185, 116, 85))
+        self.screen.blit(waiting_surface,(640 - waiting_surface.get_width()//2 , 360 - waiting_surface.get_height()//2))
+        self.screen.blit(welcome_surface, (640 - welcome_surface.get_width()//2, 0))
+        self.screen.blit(player_surface, (0, 0))
+        pygame.display.update()
+
+
 
     def handle_play(self):
         pass
